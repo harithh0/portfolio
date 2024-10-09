ec63e32c9382b065693cab5d2b2b0ea2c7529158
harithh0 <169714068+harithh0@users.noreply.github.com>
2024-10-09 09:41:07 -0500

    final

--- components/homepage/BlogLinks.tsx
+++ components/homepage/BlogLinks.tsx
@@ -8,7 +8,7 @@ export function BlogLinks() {
 
   return (
     <div className="flex flex-col space-y-1.5">
-      <Link href="/projects" className="hover:underline">
+      {/* <Link href="/projects" className="hover:underline">
         <Twemoji emoji="hammer-and-wrench" />
         <span data-umami-event="home-link-projects" className="ml-1.5">
           {t('menu_projects_2')}
@@ -19,7 +19,7 @@ export function BlogLinks() {
         <span data-umami-event="home-link-blog" className="ml-1.5">
           {t('menu_blog_2')}
         </span>
-      </Link>
+      </Link> */}
 
       {/* <Link href="/snippets" className="hover:underline">
         <Twemoji emoji="dna" />
@@ -34,12 +34,12 @@ export function BlogLinks() {
           {t('menu_about_2')}
         </span>
       </Link> */}
-      <Link href="/resume" className="hover:underline">
+      {/* <Link href="/resume" className="hover:underline">
         <Twemoji emoji="briefcase" />
         <span data-umami-event="home-link-resume" className="ml-1.5">
           {t('menu_resume_2')}
         </span>
-      </Link>
+      </Link> */}
 
       {/* <Link href={siteMetadata.analyticsURL} className="hover:underline">
         <Twemoji emoji="bar-chart" />
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 export let headerNavLinks = [
   { href: '/blog', label: 'menu_blog', name: 'Blog' },
   // { href: '/snippets', label: 'menu_snippets' },
-  { href: '/projects', label: 'menu_projects', name: 'Projects' },
+  // { href: '/projects', label: 'menu_projects', name: 'Projects' },
   { href: '/about', label: 'menu_about', name: 'About' },
-  { href: '/resume', label: 'menu_resume', name: 'Resume' },
+  // { href: '/resume', label: 'menu_resume', name: 'Resume' },
 ]

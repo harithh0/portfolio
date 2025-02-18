4d5a6dd79845b4833086f8791d2d1e36aeba0933
harithh0 <169714068+harithh0@users.noreply.github.com>
2025-02-18 11:31:10 -0600

    before removing resume

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,6 +2,6 @@ export let headerNavLinks = [
   { href: '/blog', label: 'menu_blog', name: 'Blog' },
   // { href: '/snippets', label: 'menu_snippets' },
   { href: '/projects', label: 'menu_projects', name: 'Projects' },
-  { href: '/about', label: 'menu_about', name: 'About' },
-  { href: '/resume', label: 'menu_resume', name: 'Resume' },
+  // { href: '/about', label: 'menu_about', name: 'About' },
+  // { href: '/resume', label: 'menu_resume', name: 'Resume' },
 ]
